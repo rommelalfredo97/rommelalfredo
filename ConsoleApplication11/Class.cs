d11db8af66eb99a3a44d48320182b8fba22c1b0e
monkey <monkey@BB21017>
2017-02-15 16:46:16 -0600

    Created a class

---
+++ ConsoleApplication11/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Class
+    {
+        //Just generally dicking around. Don't mind me. It's what I do.
+    }
+}
